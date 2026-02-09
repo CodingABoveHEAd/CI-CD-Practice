ddfabd8052e9ab3f2a69a0be7213ce12f4a95254
niloy <chowdhuryniloy819@gmail.com>
2026-02-10 01:51:57 +0600

    added two more key-values

--- api/main.js
+++ api/main.js
@@ -5,8 +5,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({ message: "Hello from Vercel version 2!" ,
     name: "Vercel API",
-        version: "2.0"
-        
+    version: "2.0",
+    build_at: new Date().toISOString(),
+
   });
 });
 
